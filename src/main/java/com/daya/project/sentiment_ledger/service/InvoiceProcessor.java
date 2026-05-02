88953c94519f0e067a9f4c6779491b2e034872f7
Dayalan D S <dayalands2210@gmail.com>
2026-05-02 17:23:19 +0530

    Kafka corrections

--- src/main/java/com/daya/project/sentiment_ledger/service/InvoiceProcessor.java
+++ src/main/java/com/daya/project/sentiment_ledger/service/InvoiceProcessor.java
@@ -2,7 +2,6 @@ package com.daya.project.sentiment_ledger.service;
 
 import com.daya.project.sentiment_ledger.config.KafkaTopicConfig;
 import com.daya.project.sentiment_ledger.model.Invoice;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -16,27 +15,18 @@ public class InvoiceProcessor {
 
     private static final Logger log = LoggerFactory.getLogger(InvoiceProcessor.class);
     private final StringRedisTemplate redisTemplate;
-    private final ObjectMapper objectMapper;
 
-    // Injecting both Redis and the JSON ObjectMapper
-    public InvoiceProcessor(StringRedisTemplate redisTemplate, ObjectMapper objectMapper) {
+    public InvoiceProcessor(StringRedisTemplate redisTemplate) {
         this.redisTemplate = redisTemplate;
-        this.objectMapper = objectMapper;
     }
 
-    // Notice we now consume a raw 'String' instead of an 'Invoice' object
     @KafkaListener(topics = KafkaTopicConfig.INVOICE_SUBMITTED_TOPIC, groupId = "ledger-processing-group")
-    public void consumeInvoiceEvent(String invoiceJson) {
+    public void consumeInvoiceEvent(Invoice invoice) {
         try {
-            // 1. Manually convert the JSON string into our Java object
-            Invoice invoice = objectMapper.readValue(invoiceJson, Invoice.class);
-
             log.info("🔥 KAFKA EVENT RECEIVED for Invoice ID: {}", invoice.getId());
 
-            // 2. Define the Idempotency Key
             String lockKey = "invoice:lock:" + invoice.getId();
 
-            // 3. The Check & Lock (Atomic Operation)
             Boolean isNewInvoice = redisTemplate.opsForValue().setIfAbsent(lockKey, "LOCKED", Duration.ofHours(24));
 
             if (Boolean.FALSE.equals(isNewInvoice)) {
@@ -49,8 +39,7 @@ public class InvoiceProcessor {
             log.info("Amount: ₹{}", invoice.getAmount());
 
         } catch (Exception e) {
-            // If the JSON is bad, we will finally see WHY!
-            log.error("❌ Failed to process Kafka message: {}", e.getMessage());
+            log.error("❌ Failed to process Kafka message: {}", e.getMessage(), e);
         }
     }
 }
